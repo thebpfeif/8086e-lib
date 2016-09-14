5d0bf5501dd19d229eef32bc44ca09813fc50bf2
Brandon Pfeifer <thebpfeifer@gmail.com>
2016-09-13 20:53:05 -0500

    -Formatted comments -Added decoder header file

---
+++ biu/biu_pub_decode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "stdint.h"
+
+/* 8086 instruction set */
+/* Key: */
+typedef uint8_t BIU_opcode; 
+enum
+{
+	ADD_RM8_R8 = 0x00, 
+	ADD_RM16_R16 = 0x01, 
+	ADD_R8_
+
+};
--- eu/eu_pub_registers.h
+++ eu/eu_pub_registers.h
@@ -12,21 +12,21 @@ enum
 	EU_SI,				/* source index					*/
 	EU_SP,				/* stack pointer				*/
 	EU_BP,				/* base pointer					*/
-	EU_REG_16_COUNT	/* total eu 16-bit reg count	*/
+	EU_REG_16_COUNT		/* total eu 16-bit reg count	*/
 };
 
 typedef uint8_t EU_reg_8_t8; 
 enum
 {
-	EU_AH,		/* high-byte accumulator			*/
-	EU_AL,		/* low-byte accumulator				*/
-	EU_BH,		/* high-byte base addresser			*/
-	EU_BL,		/* low-byte base addresser			*/
-	EU_CH,		/* high-byte looper counter			*/
-	EU_CL,		/* low-byte looper counter			*/
-	EU_DH,		/* high-byte data pointer			*/
-	EU_DL,		/* low-byte data pointer			*/
-	EU_REG_8_COUNT /* total eu 8-bit reg count	*/
+	EU_AH,				/* high-byte accumulator			*/
+	EU_AL,				/* low-byte accumulator				*/
+	EU_BH,				/* high-byte base addresser			*/
+	EU_BL,				/* low-byte base addresser			*/
+	EU_CH,				/* high-byte looper counter			*/
+	EU_CL,				/* low-byte looper counter			*/
+	EU_DH,				/* high-byte data pointer			*/
+	EU_DL,				/* low-byte data pointer			*/
+	EU_REG_8_COUNT		/* total eu 8-bit reg count			*/
 };
 
 /* PROCEDURES */
